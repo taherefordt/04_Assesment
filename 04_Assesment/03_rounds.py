13c717a3766f8ad3f4c5a29dcf5b79096a3d6684
taherefordt <94874903+taherefordt@users.noreply.github.com>
2022-08-02 09:38:29 +1200

    Add files via upload

---
+++ 04_Assesment/03_rounds.py
@@ -0,0 +1,54 @@
+
+# Number checking function goes here
+def intcheck(question, low=None, high=None, exit_code=None):
+
+    while True:
+
+        # sets up error messages
+        if low is not None and high is not None:
+            error = "Please enter an integer between {} and {} (inclusive)".format(low, high)
+        elif low is not None:
+            error = "Please enter an integer that is more than or equal to {}".format(low)
+        elif high is not None:
+            error = "Please enter an integer that is less than or equal to {}".format(high)
+        else:
+            error = "Please enter an integer"
+
+        try:
+            response = input(question)
+            
+            # check to see if response is the exit code and return it
+            if response == exit_code:
+                return response
+            
+            # change the response into an integer
+            else:
+                response = int(response)
+
+            # Checks response is not too low, not use of 'is not' keywords
+            if low is not None and response < low:
+                print(error)
+                continue
+
+            # Checks response is not too high
+            if high is not None and response > high:
+                print(error)
+                continue
+
+            return response
+
+        # checks input is a integer
+        except ValueError:
+            print(error)
+            continue
+
+questions_total = intcheck("How many questions? ", 1)
+questions_played = 0
+
+
+while questions_total > questions_played:
+
+    print("this is a round")
+    questions_played += 1
+
+    
